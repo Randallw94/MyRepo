c662858d35e5bc1c035c61ed3a4b9000f99b58f9
James R <randallw@email.uscb.edu>
2022-10-24 23:04:44 -0400

    Functions Basic I
    completed 1 -13 left the two other left open since the concept is the same throughout

---
+++ Python_Fundamentals/Python_Fundamentals/functions_basic_I.py
@@ -0,0 +1,125 @@
+# For each of the following code snippets, first predict the output (what you will see printed to the terminal)
+
+# 1. my prediction will return 5
+# def number_of_food_groups():
+#     return 5
+# print(number_of_food_groups())
+
+#2 my prediction will be an error since one function doesnt exist
+# def number_of_military_branches():
+#     return 5
+# print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+
+#3 my prediction will return 5 since the first return statement is taken place 
+# def number_of_books_on_hold():
+    # return 5
+    # return 10
+# print(number_of_books_on_hold())
+
+#4 my prediction will return 5 but not ten since the return statement is executed first
+# def number_of_fingers():
+#     return 5
+#     print(10)
+# print(number_of_fingers())
+
+# #5 my prediction will return 5 with no return statement will show up as none
+# def number_of_great_lakes():
+#     print(5)
+# x = number_of_great_lakes()
+# print(x)
+
+#6 my prediction will return an error since nothing is being saved 
+# def add(b,c):
+#     print(b+c)
+# print(add(1,2) + add(2,3))
+
+#7 my prediction will return 25 since the values are being used as strings 
+# def concatenate(b,c):
+#     return str(b)+str(c)
+# print(concatenate(2,5))
+
+#8 will print 100 first and return 10
+# def number_of_oceans_or_fingers_or_continents():
+#     b = 100
+#     print(b)
+#     if b < 10:
+#         return 5
+#     else:
+#         return 10
+#     return 7
+# print(number_of_oceans_or_fingers_or_continents())
+
+#9
+# def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
+#     if b<c:
+#         return 7
+#     else:
+#         return 14
+#     return 3
+# print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
+# will return 7
+# print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+# will return 14
+# print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+#will return 21
+
+#10 will reutrn 8 and disregard the second return statement
+# def addition(b,c):
+#     return b+c
+#     return 10
+# print(addition(3,5))
+
+#11
+# b = 500
+# print(b)
+# will return 500
+# def foobar():
+#     b = 300
+#     print(b)
+# print(b)
+# will return 500
+# foobar()
+# will print out 300
+# print(b)
+# will return 500
+
+#12
+# b = 500
+# print(b)
+# will return 500
+# def foobar():
+#     b = 300
+#     print(b)
+#     return b
+# print(b)
+# will print out 500 since we are not calling the function
+# foobar()
+# will print out 300 
+# print(b)
+# will print out 500
+
+#13
+# b = 500
+# print(b)
+# def foobar():
+#     b = 300
+#     print(b)
+#     return b
+# print(b)
+# b=foobar()
+# print(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
