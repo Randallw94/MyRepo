466b881efc89ba37e654f969d81506cc1e708b4d
James R <randallw@email.uscb.edu>
2022-11-07 15:09:40 -0500

    Flask Fundamentals
    install flask and ran it on the server

---
+++ Python_Fundamentals/Flask/flask_fundamentals/hello_flask/server.py
@@ -0,0 +1,15 @@
+# Import Flask to allow us to create our app
+from flask import Flask
+
+# Create a new instance of the Flask class called "app"
+app = Flask(__name__)
+
+# The "@" decorator associates this route with the function immediately following
+@app.route('/')
+def hello_world():
+    # Return the string 'Hello World!' as a response
+    return 'Hello World!'
+# Ensure this file is being run directly and not from a different module
+if __name__ == '__main__':
+    # Run the app in debug mode.
+    app.run(debug=True)
